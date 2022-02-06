0aa22f2224d65562286c6d749ca95113ed8b64b8
Muath Alhumaid <MAlhumaid@aas.com.sa>
2022-02-06 11:43:40 +0300

    license

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import License from './License/licenseSection'
+import LicenseAction from './License/licenseActions'
+
+
+function App() {
+  return (
+    <div className="App">
+   
+        <License />
+        <LicenseAction />
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/License/licenseActions.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const LicenseActions = () => {
+    
+
+    return( 
+        
+        
+        <footer className="actions-row">
+            <a href="medad-license.html?bmFtZT3YrNin2YXYudipINin2YTZhdmE2YMg2LPYudmI2K8mYWRkcmVzcz3Yp9mE2LHZitin2LYgLNin2YTZhdmF2YTZg9ipINin2YTYudix2KjZitipINin2YTYs9i52YjYr9mK2KkmcmVkaXJlY3Q9aHR0cHM6Ly9pbHMtZGV2Lm1lZGFkLmNvbS8=" id="redirect" className="agree-button"  > موافقة </a>
+        </footer>
+ )
+ 
+}
+
+
+export default LicenseActions;
